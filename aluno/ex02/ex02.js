06c3234d682b53b91cfa1006608b5cf1527048a9
LVDev <lucas.fst.dev@gmail.com>
2026-07-28 15:03:15 -0300

    Exercicios de condicionais (2 a 7)

---
+++ aluno/ex02/ex02.js
@@ -0,0 +1,10 @@
+let senha = "Lucas123";
+let senhaUser;
+
+senhaUser = prompt("digite uma senha")
+
+if(senhaUser == senha){
+    alert("Parabéns a senha está correta !")
+}else{
+    alert("A senha está incorreta")
+}
