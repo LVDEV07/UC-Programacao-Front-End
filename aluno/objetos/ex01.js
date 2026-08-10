9eac7601313202ea39f3a9dffa824857730d27e6
Lucas <lucas.fst.dev@gmail.com>
2026-08-10 15:50:27 -0300

    feat: ex 1 - Produto mais caro

---
+++ aluno/objetos/ex01.js
@@ -0,0 +1,34 @@
+/*
+1. Escreva um programa que declara dois objetos literais `produto`, com as propriedades `nome (string)`, `preco (number)`, `categoria (string | "Laticínios", "Hortifruti", "Pães", "Bebidas", etc.)`. Escreva uma função que recebe dois produtos como parâmetros e exibe no console qual é o produto mais caro, seu nome e categoria.
+*/
+
+
+const produto1 = {
+    nome: "Banana prata",
+    preco: 5.49,
+    categoria: "Laticínios"
+};
+ 
+const produto2 = {
+    nome: "Laranja Bahia",
+    preco: 12.90,
+    categoria: "Frutas"
+};
+ 
+function produtoMaisCaro(produtoA, produtoB) {
+    let maisCaro;
+ 
+    if (produtoA.preco >= produtoB.preco) {
+        maisCaro = produtoA;
+    } else {
+        maisCaro = produtoB;
+    }
+ 
+    console.log(
+        `O produto mais caro é "${maisCaro.nome}" (categoria: ${maisCaro.categoria}), ` +
+        `custando R$ ${maisCaro.preco}`
+    );
+    return maisCaro;
+}
+ 
+produtoMaisCaro(produto1, produto2);
